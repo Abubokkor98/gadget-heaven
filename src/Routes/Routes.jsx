7eec256b8db8da77bdc31d613b4e09bf82e67da2
Abubokkor98 <mail.abubokkor@gmail.com>
2024-11-05 19:00:37 +0600

    navbar, fotter and banner added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,45 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
+import Home from "../Pages/Home";
+import Statistics from "../Components/Statistics";
+import Dashboard from "../Components/Dashboard";
+import History from "../Components/History";
+import Category from "../Components/Category";
+
 
 
 const routes = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout></MainLayout>,
+      children:[
+        {
+          path: "/",
+        element: <Home></Home>,
+        loader: () => fetch("../categories.json"),
+        children: [
+          {
+            path: "/category/:categoryId",
+            element:<Category></Category>,
+          },
+        ],
+        },
+        
+        {
+          path: "/statistics",
+          element: <Statistics></Statistics>,
+        },
+        {
+          path: "/dashboard",
+          element: <Dashboard></Dashboard>,
+        },
+        {
+          path: "/history",
+          element: <History></History>,
+        },
+      ]
+      
+      
     },
   ]);
 
