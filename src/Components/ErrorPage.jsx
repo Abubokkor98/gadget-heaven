c492c3db65d610bcd5162a0bde22df31f5dbe777
Abubokkor98 <mail.abubokkor@gmail.com>
2024-11-05 20:11:26 +0600

    error page added

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import { Link, useRouteError } from "react-router-dom";
 
-export default function ErrorPage() {
+const ErrorPage = () => {
+  const error = useRouteError();
+  console.error(error);
+
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <>
+      <div
+        id="error-page"
+        className="flex flex-col justify-center items-center h-screen space-y-5"
+      >
+        <h1 className="text-8xl text-purple-600 font-bold">Oops!</h1>
+        <p className="text-2xl font-semibold">
+          Sorry, an unexpected error has occurred.
+        </p>
+        <p className="text-3xl space-x-2 font-bold">
+          <i>{error.statusText || error.message}</i>
+          <span>404</span>
+        </p>
+        <Link
+          to="/"
+          className="btn bg-purple-600 text-white px-10 rounded-full"
+        >
+          Goo Back
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default ErrorPage;
